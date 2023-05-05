f2bc87b7a4d356c534e54ba80b36e4d301257400
Azka norouzi <azka@azkas-MacBook-Air.local>
2023-05-05 14:35:30 +0330

    implement local storage

--- js/app.js
+++ js/app.js
@@ -10,25 +10,37 @@ const APPLICATION = {
   titleInput: document.querySelector('.title-input'),
   pagesInput: document.querySelector('.pages-input'),
   completedPagesInput: document.querySelector('.completed-pages-input'),
-  imageUrlInput: document.querySelector('.image-url-input'),
   moduleAddButton: document.querySelector('.module-add'),
+  gridContainer: document.querySelector('.grid-container'),
+  domBooks: document.querySelectorAll('.dom-book'),
 }
 const HELPER_FUNCTIONS = {
   isValidBook(bookInfo) {
-    return Object.keys(bookInfo).length === 5
+    return Object.keys(bookInfo).length === 4
   },
 }
 // Stores book information before finalization
 let newBookInfo = {}
 // Stores all book's information
-const BOOKS = []
+let BOOKS = []
+// Local storage
+// saving the BOOKS object inside localStorage each time user closes the tab
+window.addEventListener('beforeunload', function () {
+  localStorage.setItem('booksHistory', JSON.stringify(BOOKS))
+})
+// rendering books that are stored in localStorage
+window.addEventListener('load', function () {
+  BOOKS = JSON.parse(this.localStorage.getItem('booksHistory'))
+  console.log(BOOKS)
+  BOOKS.forEach((book) => renderDomBook(createDomBook(book)))
+})
+
 // Hamburger menu settings
 APPLICATION.toggleButton.addEventListener('click', () => {
   document.querySelector('aside').classList.toggle('-translate-x-full')
 })
 // Plus sign settings
 APPLICATION.plusContainer.addEventListener('click', () => {
-  console.log('hey')
   APPLICATION.module.classList.remove('module-hidden')
   document.body.classList.add('overflow-hidden')
   // Scrolling to the top of the page
@@ -47,11 +59,11 @@ APPLICATION.module.addEventListener('click', (event) => {
 })
 // Setting max completed pages to the number of book's pages
 APPLICATION.pagesInput.addEventListener('blur', () => {
-  newBookInfo.MaxPages = +APPLICATION.pagesInput.value
+  newBookInfo.maxPages = +APPLICATION.pagesInput.value
   // Don't allowing the number of pages to go less than 10
-  if (newBookInfo.MaxPages < 10) {
+  if (newBookInfo.maxPages < 10) {
     document.querySelector('.pages-input').value = '10'
-    newBookInfo.MaxPages = 10
+    newBookInfo.maxPages = 10
   }
 })
 APPLICATION.completedPagesInput.addEventListener('blur', () => {
@@ -60,10 +72,10 @@ APPLICATION.completedPagesInput.addEventListener('blur', () => {
   )
   console.log(newBookInfo.initialCompletedPages)
   // In case user enters a number that's greater than specified max-pages of the book
-  if (+newBookInfo.initialCompletedPages > +newBookInfo.MaxPages) {
+  if (+newBookInfo.initialCompletedPages > +newBookInfo.maxPages) {
     document.querySelector('.completed-pages-input').value =
-      newBookInfo.MaxPages
-    newBookInfo.initialCompletedPages = newBookInfo.MaxPages
+      newBookInfo.maxPages
+    newBookInfo.initialCompletedPages = newBookInfo.maxPages
   }
   // In case user enters a number that's less than 0 or includes none-numerical characters
   if (
@@ -74,20 +86,6 @@ APPLICATION.completedPagesInput.addEventListener('blur', () => {
     newBookInfo.initialCompletedPages = 0
   }
 })
-APPLICATION.imageUrlInput.addEventListener('blur', () => {
-  console.log(APPLICATION.imageUrlInput)
-  newBookInfo.imageUrl = APPLICATION.imageUrlInput.value.trim()
-  // adds https:// if http:// or https:// is omitted
-  if (
-    !newBookInfo.imageUrl.startsWith('https://') &&
-    !newBookInfo.imageUrl.startsWith('http://') &&
-    newBookInfo.imageUrl
-  ) {
-    const fixedUrl = 'https://' + newBookInfo.imageUrl
-    document.querySelector('.image-url-input').value = fixedUrl
-    newBookInfo.imageUrl = fixedUrl
-  }
-})
 // Module add button
 APPLICATION.moduleAddButton.addEventListener('click', (e) => {
   newBookInfo.title = APPLICATION.titleInput.value || 'Untitled'
@@ -95,16 +93,31 @@ APPLICATION.moduleAddButton.addEventListener('click', (e) => {
   if (HELPER_FUNCTIONS.isValidBook(newBookInfo)) {
     APPLICATION.module.classList.add('module-hidden')
     APPLICATION.newBookForm.reset()
-    BOOKS.push({ ...newBookInfo })
+    const newBook = new Book(
+      newBookInfo.title,
+      newBookInfo.name,
+      newBookInfo.maxPages,
+      newBookInfo.initialCompletedPages
+    )
+    renderDomBook(createDomBook(newBook))
+    BOOKS.push(newBook)
+    changeBookInformation()
     newBookInfo = {}
   }
+  console.log(BOOKS)
 })
-function Book(title, author, pages, completedPages, imageUrl) {
+function Book(title, author, pages, completedPages) {
   this.title = title
   this.author = author
   this.pages = pages
   this.completedPages = completedPages
-  this.imageUrl = imageUrl
+  // This will prevent equivalent ids
+  let randomNumber =
+    Math.trunc(Math.random() * (9999999 - 1000000 + 1)) + 1000000
+  while (BOOKS.find((book) => book.id === randomNumber)) {
+    randomNumber = Math.trunc(Math.random() * (9999999 - 1000000 + 1)) + 1000000
+  }
+  this.id = randomNumber
 }
 // Returns current number of books
 Book.getCurrentNumber = function () {
@@ -114,19 +127,15 @@ Book.getCurrentNumber = function () {
 Book.getCurrentFinishedNumber = function () {
   return BOOKS.filter((book) => book.completedPages === book.pages).length
 }
-// Returns current number of finished books
+// Returns total number of pages
 Book.getCurrentPages = function () {
-  return BOOKS.filter((book) => book.completedPages === book.pages).length
-}
-// Returns total number of finished pages
-Book.getCurrentFinishedPages = function () {
   return BOOKS.reduce((sum, book) => {
-    sum += book.
+    sum += book.pages
     return sum
   }, 0)
 }
 // Returns total number of finished pages
-Book.getCurrentFinishedNumber = function () {
+Book.getCurrentFinishedPages = function () {
   return BOOKS.reduce((sum, book) => {
     sum += book.completedPages
     return sum
@@ -151,4 +160,81 @@ Book.prototype.edit = function (property, newValue) {
 Book.prototype.done = function () {
   this.completedPages = this.pages
   return this
+}
+// Returns a new book with the given properties from user
+function createDomBook(book) {
+  book.rendered = true
+  return `<article
+  class="grid-item bg-slate-800 text-white relative overflow-hidden border-2 border-orange-600 flex justify-center items-center dom-book"
+  data-id="${book.id}"
+>
+  <div
+    class="absolute top-0 left-0 right-0 p-2 flex justify-between text-xl bg-slate-700 bg-opacity-90 border-b-4 border-orange-600"
+  >
+    <button class="text-right close-button" type="button">❌</button>
+  </div>
+  <div class="book-info text-center">
+    <h1 class="text-2xl book-title">${book.title}</h1>
+    <p>By <span class="author">${book.author}</span></p>
+    <p class="text-xl text-orange-600">
+      <span class="completed-page">${book.completedPages}</span> /
+      <span class="pages">${book.pages}</span>
+    </p>
+  </div>
+  <div
+    class="w-full absolute bottom-0 left-0 right-0 z-10 bg-orange-600 flex"
+  >
+    <button class="flex-1 p-2 bg-blue-900 subtract-button" type="button">-</button>
+    <button class="flex-1 p-2 bg-orange-400 add-button" type="button">+</button>
+    <button class="flex-1 p-2 bg-orange-900 done" type="button">DONE</button>
+  </div>
+</article>`
+}
+function renderDomBook(domBook) {
+  APPLICATION.gridContainer.insertAdjacentHTML('afterbegin', domBook)
+  APPLICATION.domBooks = document.querySelectorAll('.dom-book')
+  listenToDomBook(APPLICATION.domBooks[0])
+}
+// Handles click events of all the domBooks
+function listenToDomBook(domBook) {
+  domBook.addEventListener('click', (e) => {
+    const bookId = e.target.closest('.dom-book').dataset.id
+    const book = BOOKS.find((b) => +b.id === +bookId)
+    // When user clicks on close button
+    if (e.target.classList.contains('close-button') && book) {
+      console.log(BOOKS.indexOf(book))
+      BOOKS.splice(BOOKS.indexOf(book), 1)
+      domBook.remove()
+      console.log(BOOKS)
+    }
+    // When user clicks on add button
+    if (e.target.classList.contains('add-button') && book) {
+      book.completedPages += book.completedPages < book.pages ? 1 : 0
+      domBook.querySelector('.completed-page').textContent = book.completedPages
+      changeBookInformation()
+    }
+    // When user clicks on add button
+    if (e.target.classList.contains('subtract-button') && book) {
+      console.log('subtract')
+      book.completedPages -= book.completedPages > 0 ? 1 : 0
+      domBook.querySelector('.completed-page').textContent = book.completedPages
+      changeBookInformation()
+    }
+    if (e.target.classList.contains('done') && book) {
+      console.log('done')
+      book.completedPages = book.pages
+      domBook.querySelector('.completed-page').textContent = book.completedPages
+      changeBookInformation()
+    }
+  })
+}
+function changeBookInformation() {
+  document.querySelector('.number-of-books').textContent =
+    Book.getCurrentNumber()
+  document.querySelector('.number-of-completed-books').textContent =
+    Book.getCurrentFinishedNumber()
+  document.querySelector('.number-of-total-pages').textContent =
+    Book.getCurrentPages()
+  document.querySelector('.number-of-completed-pages').textContent =
+    Book.getCurrentFinishedPages()
 }
